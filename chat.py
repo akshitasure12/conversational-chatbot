c23e6a32fa39e50558d04cd658954f1666c0c672
Akshita Sure <sureakshita23@gmail.com>
2025-05-09 00:03:38 +0530

    Added a modification to run the trained model on the local device

--- chat.py
+++ chat.py
@@ -8,7 +8,7 @@ with open('intents.json', 'r') as f:
     intents = json.load(f)
     
 FILE = "data.pth"
-data = torch.load(FILE)
+data = torch.load(FILE, map_location=torch.device('cpu'))
 
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
